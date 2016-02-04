eb80c76d2dd5d4190a71562ff919f7d16cc41a18
justinask <justinas.krutulis@gmail.com>
2016-02-05 00:11:12 +0200

    Lesson one

--- src/main/java/org/gradle/poetry/Poetry.java
+++ src/main/java/org/gradle/poetry/Poetry.java
@@ -1,6 +1,5 @@
 package org.gradle.poetry;
 
-import java.lang.Object;
 import java.lang.String;
 import java.lang.System;
 import java.util.ArrayList;
@@ -10,13 +9,22 @@ public class Poetry {
 
     public List<String> juliusCaesar() {
         List<String> lines = new ArrayList<>();
-        lines.add("0, pardom me, thou");
+        lines.add("O, pardom me, thou");
         lines.add("That i am meek and gentle");
+        lines.add("O, pardon me, thou bleeding piece of earth,");
+        lines.add("That I am meek and gentle with these butchers!");
+        lines.add("Thou art the ruins of the noblest man");
+        lines.add("That ever livèd in the tide of times.");
+        lines.add("Woe to the hand that shed this costly blood!");
+        lines.add("  Over thy wounds now do I prophesy—");
+        lines.add("Which, like dumb mouths, do ope their ruby lips");
+        lines.add("To beg the voice and utterance of my tongue—");
+        lines.add("A curse shall light upon the limbs of men.");
     }
 
-    public  void emit(List<String> lines){
+    public void emit(List<String> lines) {
         for (String line : lines) {
-            System.console().printf(line)
+            System.console().printf(line);
         }
     }
 
